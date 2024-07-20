8c7774ad3c3c54e77993cc57078e402a3b4e5976
prayansh <parmarprayansh2@gmail.com>
2024-07-20 10:36:45 +0530

    Remove dist folder from tracking

--- dist/src/routes/tasks.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (router) => {
-    // router.post('/api/v1/tasks',isAuthenticated,createTask);
-    // router.get('/api/v1/tasks',isAuthenticated,getTasks);
-    // router.get('/api/v1/tasks/:taskid',isAuthenticated,getTasksById);
-    // router.put('/api/v1/tasks/:taskid',isAuthenticated,updateTask);
-    // router.delete('/api/v1/tasks/:taskid',isAuthenticated,deleteTask);
-};
